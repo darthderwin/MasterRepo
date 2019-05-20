cb226e834b287cb1c9d8229baa46cac3977a7508
darthderwin <45957544+darthderwin@users.noreply.github.com>
2019-05-20 14:01:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public static bool isPangram(string s)
+        {
+            HashSet<char> hS = new HashSet<char>();
+            foreach (char x in s)
+            {
+                hS.Add(newClass sb, char x);
+            }
+            return true;
+        }
+        public static void newClass()
+        {
+            string sb = "";
+        }
+    }
+}
+
+// 1. 
+// isPangram(string s)
+// Add Char (_____sb, char x);
+// isPangram(sb.GetAsString);
+
+// 2. Insertion sort
+// 3. Merge sort
+// 4. Quicksort
+// 5. Dijksins shorthest path
